793fd891d225ec0b94c04c2b581dfada53a209a9
jtiwakiri <jtiwakiri@gmail.com>
2017-12-06 07:23:12 -0800

    removed test methods

--- src/Database.java
+++ src/Database.java
@@ -6,46 +6,6 @@ public class Database {
     private Connection connection;
     Statement statement;
 
-    public static void main(String[] args) {
-        Database d = new Database("database_user", "datapassword", "localhost",
-                "cs435_lab4");
-        d.printList(d.getSchedule("Stop Address 1", "Stop Address 10", "2017-12-01"));
-        d.printList(d.getTripStopInfo("1"));
-        d.printList(d.getTripStopInfo("4"));
-        System.out.println(d.addTripOffering("4", "2017-12-01", "8:00",
-                "8:15", "Driver Name 1", "1"));
-        d.printList(d.getSchedule("Stop Address 8", "Stop Address 4", "2017-12-01"));
-        System.out.println(d.deleteTripOffering("4", "2017-12-01", "8:00"));
-        d.printList(d.getSchedule("Stop Address 8", "Stop Address 4", "2017-12-01"));
-        d.updateTripOfferingDriver("4", "2017-12-01", "8:30", "newdriver");
-        d.printList(d.getSchedule("Stop Address 8", "Stop Address 4", "2017-12-01"));
-        d.updateTripOfferingDriver("4", "2017-12-01", "8:30", "Driver Name 7");
-        d.printList(d.getSchedule("Stop Address 8", "Stop Address 4", "2017-12-01"));
-        d.updateTripOfferingBus("4", "2017-12-01", "8:30", "1");
-        d.printList(d.getSchedule("Stop Address 8", "Stop Address 4", "2017-12-01"));
-        d.updateTripOfferingBus("4", "2017-12-01", "8:30", "2");
-        d.printList(d.getSchedule("Stop Address 8", "Stop Address 4", "2017-12-01"));
-        d.printList(d.getDriverWeeklySchedule("Driver Name 7", "2017-12-01"));
-        d.addBus("8", "Bus Model 5", "2003");
-        d.printList(d.getBusTable());
-        d.deleteBus("8");
-        d.printList(d.getBusTable());
-        System.out.println(d.addDriver("test name", "2222222222"));
-        d.printList(d.getDriverTable());
-        System.out.println(d.deleteDriver("test name"));
-        d.printList(d.getDriverTable());
-        d.addTripOffering("4", "2017-12-04", "10:00", "10:15",
-                "Driver Name 3", "2");
-        d.addActualTripStopInfo("4","2017-12-04","10:00","8",
-                "10:00","10:00","10:00","8","0");
-        d.addActualTripStopInfo("4","2017-12-04","10:00","6",
-                "10:08","10:00","10:10","5","2");
-        d.addActualTripStopInfo("4", "2017-12-04", "10:00", "4",
-                "10:15", "10:00", "10:20", "0", "9");
-        d.printList(d.getActualTripStopInfoTable());
-        System.out.println(d.deleteTripOffering("4", "2017-12-04", "10:00"));
-    }
-
     public Database(String user, String password, String host, String databaseName) {
         String connectionString = "jdbc:mysql://" + host + "/" + databaseName + "?user="
                 + user + "&password=" + password;
@@ -63,42 +23,7 @@ public class Database {
             e.printStackTrace();
         }
     }
-
-    public String test(String table) {
-        StringBuilder builder = new StringBuilder();
-        try {
-            // Query
-            ResultSet results = statement.executeQuery("SELECT * from " + table + ";");
-            // Metadata
-            ResultSetMetaData meta = results.getMetaData();
-            int colNum = meta.getColumnCount();
-            // Get results
-            for (int i = 1; i <= colNum; i++) {
-                builder.append(meta.getColumnLabel(i) + '\t');
-            }
-            builder.append('\n');
-            while (results.next()) {
-                for (int i = 1; i <= colNum; i++) {
-                    builder.append(results.getString(i) + '\t');
-                }
-                builder.append('\n');
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return builder.toString();
-    }
-
-    public void printList(List<String[]> l) {
-        for (int i = 0; i < l.size(); i++) {
-            String[] entry = l.get(i);
-            for (int j = 0; j < entry.length; j++) {
-                System.out.print(entry[j] + "\t\t");
-            }
-            System.out.println();
-        }
-    }
-
+    
     public List<String[]> getSchedule(String start, String dest, String date) {
         String query = "SELECT T.StartLocationName, T.DestinationName, O.TripDate,"
                 + " O.ScheduledStartTime, O.ScheduledArrivalTime, O.DriverName, O.BusID"
